30696fa76510b3a4a8c0241372caeb22f90d02fa
ank-21 <ankitsrivastava21598@gmail.com>
2024-04-04 23:28:16 +0530

    added memento design pattern

--- mementoPattern/src/Client.java
+++ mementoPattern/src/Client.java
@@ -1,6 +1,6 @@
 public class Client {
 
-    public static void main(String args[]){
+    public static void main(String[] args){
         ConfigurationCaretaker caretaker = new ConfigurationCaretaker();
 
         //initiate state of originator
